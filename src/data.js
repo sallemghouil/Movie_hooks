c9c4aca103b746e197ff84a8fe284ed0c44c83c4
sallemghouil <06243299ghouil>
2022-06-02 13:54:37 +0100

    first commit

--- src/data.js
+++ src/data.js
@@ -3,7 +3,7 @@ export const movies = [
         id:0,
         "title": "Kingsglaive",
         "description": "King Regis, who oversees the land of Lucis, commands his army of soldiers to protect the kingdom from the Niflheim empire's plans to steal the sacred crystal.",
-        "posterUrl": "./images/Kingsglaive_Final_Fantasy_XV.jpg",
+        "posterUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTtJ7ph0SpDdKxZ9GFkk36g5XnEf_8In_2i-Q&usqp=CAU",
         "rating": 4,
         "trailerSrc": "https://www.youtube.com/embed/htnkOpknGok"
     },
@@ -11,7 +11,7 @@ export const movies = [
         id:1,
         "title": "Final Fantasy",
         "description": "A scientist makes a last stand on Earth with the help of a ragtag team of soldiers against an invasion of alien phantoms.",
-        "posterUrl": "../images/Final_Fantasy_Spirits_Within.jpg",
+        "posterUrl": "https://encrypted-tbn0.gstatic.com/images q=tbn:ANd9GcT19oVKeEloU3_cgrXA0qlT0dDyYYLZ_ESemw&usqp=CAU",
         "rating": 1,
         "trailerSrc": "https://www.youtube.com/embed/4tyuIh12_HU"
     },
@@ -19,7 +19,7 @@ export const movies = [
         id:2,
         "title": "Ghost In The Shell",
         "description": "A hacker known as the Puppet Master is hunted by a female cyborg cop and her partner. This film is a revised version of Ghost in the Shell (1995).",
-        "posterUrl": "../images/Ghost_In_The_Shell_2_0.jpg",
+        "posterUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJUU9r9sZUfmZ99fcfc0F1HXC-tS_XvxAuuQ&usqp=CAU",
         "rating": 5,
         "trailerSrc": "https://www.youtube.com/embed/G4VmJcZR0Yg"
     },
@@ -27,7 +27,7 @@ export const movies = [
         id:3,
         "title": "Appleseed Alpha",
         "description": "A young female soldier Deunan and her cyborg partner Briareos survive through the post World War 3 apocalyptic New York in search of human's future hope.",
-        "posterUrl": "../images/Appleseed_Alpha.jpg",
+        "posterUrl": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQHt9vVbmTZAk586apnVXL_3TMwJ9MQnNFnCA&usqp=CAU",
         "rating": 4,
         "trailerSrc": "https://www.youtube.com/embed/4DtWCjogd1M"
     },
@@ -35,7 +35,7 @@ export const movies = [
         id:4,
         "title": "Resident Evil",
         "description": "Chris Redfield enlists the help of Leon S. Kennedy and Rebecca Chambers to stop a death merchant, with a vengeance, from spreading a deadly virus in New York.",
-        "posterUrl": "../images/Resident_Evil_Vendetta.jpg",
+        "posterUrl": "https://freakingeek.com/wp-content/uploads/2017/08/ResidentEvilVendetta-02-800x445.jpg",
         "rating": 2,
         "trailerSrc": "https://www.youtube.com/embed/W1OUs3HwIuo"
     }
